2068987b991a15aee36ebf4b8450b8614fc98e47
c-razo <subscribemecr@gmail.com>
2024-11-17 13:05:31 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    tools = "Here are the tools you can use for remote work!"  # You can dynamically generate this data
+    tools = "Here are the tools you can use for remote work!"  # Can be dynamically generated
     return render_template('index.html', tools=tools)
 
 @app.route('/run_checks', methods=['POST'])
@@ -17,12 +17,15 @@ def run_checks():
     upload_speed = st.upload() / 10**6  # Convert from bits to megabits
     ping = st.results.ping
 
-    # Return the results as JSON
-    return jsonify({
+    # Store the results in a dictionary to be passed to the template
+    results = {
         'download_speed': download_speed,
         'upload_speed': upload_speed,
         'ping': ping
-    })
+    }
+
+    # Return the results and render the index template with results
+    return render_template('index.html', results=results)
 
 if __name__ == '__main__':
     app.run(debug=True)
